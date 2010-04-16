cda8ba5897c21633d3389230b9b4e650a6b50587
Michael Berkowitz <michael.berkowitz@gmail.com>
2010-04-16 10:37:17 -0400

    Adds tests and method to generate a set of dice from a Roller#description.

--- Tests/roller_spec.rb
+++ Tests/roller_spec.rb
@@ -44,4 +44,24 @@ describe "Roller" do
       end
     end
   end
+  describe "parsing rolls from descriptions" do
+    describe "a simple roll" do
+      before { @roll = Roller.from_description("D6") }
+      it "should contain a single D6" do
+        @roll.dice.should == [Die.new]
+      end
+    end
+    describe "a slightly more complex roll" do
+      before { @roll = Roller.from_description("3D20") }
+      it "should contain 3 D20" do
+        @roll.dice.should == [Die.new(20)] * 3
+      end
+    end
+    describe "a mixed roll" do
+      before { @roll = Roller.from_description("2D4, D6, 4D10, 3D100") }
+      it "should contain 2 D4, 1 D6, 4 D10, and 3 D100" do
+        @roll.dice.should == [Die.new(4)] * 2 + [Die.new] + [Die.new(10)] * 4 + [Die.new(100)] * 3
+      end
+    end
+  end
 end
